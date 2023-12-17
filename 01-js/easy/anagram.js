3d97282eb8ac9d39564a2e458ae5f80bc3b7a201
nameIsAniket <aniketlakde7262@gmail.com>
2023-12-17 12:16:21 +0530

    anagramSolution by sorting

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,13 @@
 */
 
 function isAnagram(str1, str2) {
-
+  str1 = str1.split('').sort().join('').toLowerCase();
+  str2 = str2.split('').sort().join('').toLowerCase();
+  console.log('string 1: ',str1,' string2: ',str2);
+  if (str1 == str2){
+    return true;
+  }
+  return false
 }
 
 module.exports = isAnagram;
