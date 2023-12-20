43e02b102a500e7bfb1a7c5b24d55f877c8f967a
nameIsAniket <aniketlakde7262@gmail.com>
2023-12-20 10:04:48 +0530

    solve findlargestElement

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,6 +6,11 @@
 */
 
 function findLargestElement(numbers) {
+    let max = numbers[0]
+    for (let number of numbers){
+        if (number > max) {max = number}
+    }
+    return max;
     
 }
 
